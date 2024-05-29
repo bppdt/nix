bafeca2f28f9f11fe8d27a7c14551cae17ad59f6
bppdt <171018765+bppdt@users.noreply.github.com>
2024-05-29 18:32:30 -0400

    Remove enable feature mounted ssh store and not before the build

--- tests/functional/mounted-ssh-max-jobs.sh
+++ tests/functional/mounted-ssh-max-jobs.sh
@@ -2,7 +2,5 @@ source common.sh
 
 clearStore
 
-enableFeatures mounted-ssh-store
-
-( nix-build ./simple.nix --builders "" --max-jobs 0 \
+(! nix-build ./simple.nix --builders "" --max-jobs 0 \
   --store 'mounted-ssh-ng://localhost') 
