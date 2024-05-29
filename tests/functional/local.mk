f5e4e7eac7f44db684f2221ac5019ed71c787074
bppdt <171018765+bppdt@users.noreply.github.com>
2024-05-29 06:43:39 -0400

    Try mounted ssh max jobs test

--- tests/functional/local.mk
+++ tests/functional/local.mk
@@ -72,6 +72,7 @@ nix_tests = \
   build-remote-trustless-should-pass-3.sh \
   build-remote-trustless-should-fail-0.sh \
   build-remote-with-mounted-ssh-ng.sh \
+  mounted-ssh-max-jobs.sh \
   nar-access.sh \
   impure-eval.sh \
   pure-eval.sh \
