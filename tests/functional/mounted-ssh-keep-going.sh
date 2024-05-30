2abe618e635d7887fffe91bd3f8376824eac89b9
bppdt <171018765+bppdt@users.noreply.github.com>
2024-05-30 06:04:47 -0400

    Add busybox arg to keep-going test

--- tests/functional/mounted-ssh-keep-going.sh
+++ tests/functional/mounted-ssh-keep-going.sh
@@ -1,10 +1,14 @@
 source common.sh
 
-clearStore
+requireSandboxSupport
+[[ $busybox =~ busybox ]] || skipTest "no busybox"
+
+enableFeatures mounted-ssh-store
 
 # It tests that it should not build the good derivation without the --keep-going flag
-(! nix-build ./mounted-ssh-keep-going.nix \
+(! nix-build -Lvf ./mounted-ssh-keep-going.nix \
   --extra-experimental-features mounted-ssh-store \
+  --arg busybox $busybox \
   --out-link $TEST_ROOT/result-from-mounted-remote \
   --store 'mounted-ssh-ng://localhost')
 
@@ -12,8 +16,10 @@ clearStore
   fail "Shouldn't have built because one derivation is failing"
 
 # It tests that it should build the good derivation as we have the --keep-going flag
-(! nix-build ./mounted-ssh-keep-going.nix --keep-going \
+(! nix-build -Lvf ./mounted-ssh-keep-going.nix \
   --extra-experimental-features mounted-ssh-store \
+  --arg busybox $busybox \
+  --keep-going \
   --out-link $TEST_ROOT/result-from-mounted-remote-new \
   --store 'mounted-ssh-ng://localhost')
 
