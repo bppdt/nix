4b0fe3101db91fcda51bcbf632d9852c47721f37
bppdt <171018765+bppdt@users.noreply.github.com>
2024-05-30 04:29:06 -0400

    Try the flag --extra-experimental-features mounted-ssh-store in the nix build and also use !

--- tests/functional/mounted-ssh-max-jobs.sh
+++ tests/functional/mounted-ssh-max-jobs.sh
@@ -2,5 +2,5 @@ source common.sh
 
 clearStore
 
-(nix-build ./simple.nix --builders "" --max-jobs 0 \
+(! nix-build ./simple.nix --extra-experimental-features mounted-ssh-store --builders "" --max-jobs 0 \
   --store 'mounted-ssh-ng://localhost') 
