36e08fb46392c1e372f8bea1795110bc5134bc1c
bppdt <171018765+bppdt@users.noreply.github.com>
2024-05-30 08:28:20 -0400

    Try echo $out and 2>&1 grep

--- tests/functional/mounted-ssh-keep-going.nix
+++ tests/functional/mounted-ssh-keep-going.nix
@@ -9,6 +9,21 @@ rec {
     # make the good derivation sleep to ensure it runs after the failing derivation
     good = mkDerivation {
         name = "good";
-        builder = "mkdir $out; sleep 5; echo Hello World! > $out/hello";
+        builder = "mkdir $out; sleep 5; echo Hello World! > $out";
     };
 }
+
+# with import ./config.nix;
+
+# rec {
+#     failing = mkDerivation {
+#         name = "failing";
+#         buildCommand = false;
+#     };
+
+#     # make the good derivation sleep to ensure it runs after the failing derivation
+#     good = mkDerivation {
+#         name = "good";
+#         builder = "mkdir $out; sleep 5; echo Hello World! > $out/hello";
+#     };
+# }
--- tests/functional/mounted-ssh-keep-going.sh
+++ tests/functional/mounted-ssh-keep-going.sh
@@ -1,27 +1,45 @@
 source common.sh
 
-requireSandboxSupport
-[[ $busybox =~ busybox ]] || skipTest "no busybox"
-
-enableFeatures mounted-ssh-store
+clearStore
 
 # It tests that it should not build the good derivation without the --keep-going flag
-(! nix-build ./mounted-ssh-keep-going.nix \
+(! (! nix-build ./mounted-ssh-keep-going.nix \
   --extra-experimental-features mounted-ssh-store \
-  --arg busybox $busybox \
-  --out-link $TEST_ROOT/result-from-mounted-remote \
-  --store 'mounted-ssh-ng://localhost')
-
-(! cat $TEST_ROOT/result-from-mounted-remote/hello | grepQuiet 'Hello World!') || \
+  --out-link $TEST_ROOT/result \
+  --store 'mounted-ssh-ng://localhost') 2>&1 | grep 'Hello World!') || \
   fail "Shouldn't have built because one derivation is failing"
 
 # It tests that it should build the good derivation as we have the --keep-going flag
-(! nix-build ./mounted-ssh-keep-going.nix \
+( (! nix-build ./mounted-ssh-keep-going.nix --keep-going \ 
   --extra-experimental-features mounted-ssh-store \
-  --arg busybox $busybox \
-  --keep-going \
-  --out-link $TEST_ROOT/result-from-mounted-remote-new \
-  --store 'mounted-ssh-ng://localhost')
+  --out-link $TEST_ROOT/result \
+  --store 'mounted-ssh-ng://localhost') 2>&1 | grep 'Hello World!') || \
+  fail "Should have built despite the errors because of '--keep-going'"
 
-(cat $TEST_ROOT/result-from-mounted-remote-new/hello | grepQuiet 'Hello World!') || \
-fail "Should have built despite the errors because of '--keep-going'"
+# source common.sh
+
+# requireSandboxSupport
+# [[ $busybox =~ busybox ]] || skipTest "no busybox"
+
+# enableFeatures mounted-ssh-store
+
+# # It tests that it should not build the good derivation without the --keep-going flag
+# (! nix-build ./mounted-ssh-keep-going.nix \
+#   --extra-experimental-features mounted-ssh-store \
+#   --arg busybox $busybox \
+#   --out-link $TEST_ROOT/result-from-mounted-remote \
+#   --store 'mounted-ssh-ng://localhost')
+
+# (! cat $TEST_ROOT/result-from-mounted-remote/hello | grepQuiet 'Hello World!') || \
+#   fail "Shouldn't have built because one derivation is failing"
+
+# # It tests that it should build the good derivation as we have the --keep-going flag
+# (! nix-build ./mounted-ssh-keep-going.nix \
+#   --extra-experimental-features mounted-ssh-store \
+#   --arg busybox $busybox \
+#   --keep-going \
+#   --out-link $TEST_ROOT/result-from-mounted-remote-new \
+#   --store 'mounted-ssh-ng://localhost')
+
+# (cat $TEST_ROOT/result-from-mounted-remote-new/hello | grepQuiet 'Hello World!') || \
+# fail "Should have built despite the errors because of '--keep-going'"
